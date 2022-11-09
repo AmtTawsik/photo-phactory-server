5f0aac2b6b4b5308babacf1244f7d1a20de1489a
Abdullah Al Mubin <amttawsik99@gmail.com>
2022-11-09 12:19:35 +0600

    Api for update section

--- index.js
+++ index.js
@@ -56,6 +56,20 @@ async function run(){
             res.send(result);
         })
 
+        app.delete('/reviews/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get('/reviews/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const review = await reviewCollection.findOne(query);
+            res.send(review);
+        })
+
         app.get('/reviews', async(req,res)=>{
             let query = {};
             if(req.query.serviceId){
